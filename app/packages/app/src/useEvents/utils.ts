10e965556ebfcaa113c2c4c00c58f861c3122227
manivoxel51 <mani@voxel51@gmail.com>
2023-10-09 17:13:42 +0300

    extended view with session for field visibility stage

--- app/packages/app/src/useEvents/utils.ts
+++ app/packages/app/src/useEvents/utils.ts
@@ -29,6 +29,7 @@ export const processState = (
   setter: ReturnType<typeof useSessionSetter>,
   state: any
 ) => {
+  console.log("processState", state);
   setter(
     "colorScheme",
     ensureColorScheme(state.color_scheme as ColorSchemeInput, {
@@ -45,4 +46,6 @@ export const processState = (
     toCamelCase(state.selected_labels) as State.SelectedLabel[]
   );
   state.spaces && setter("sessionSpaces", state.spaces);
+  state.field_visibility_stage &&
+    setter("fieldVisibilityStage", state.field_visibility_stage);
 };
